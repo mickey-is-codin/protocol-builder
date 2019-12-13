8ebd9cfcd2490783f0736630689c53911fb181b6
Mickey Smith <smithmick14@gmail.com>
2019-12-13 09:30:14 -0600

    Split into functions

--- src/builder.py
+++ src/builder.py
@@ -2,28 +2,39 @@ import xlsxwriter
 
 def main():
 
+    print_welcome()
+    protocol_name = get_name()
+
+    workbook  = xlsxwriter.Workbook(f"{protocol_name}.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    conditions = get_conditions()
+    create_header(worksheet, conditions)
+
+    workbook.close()
+
+def print_welcome():
     welcome_message = "Welcome to ProtocolBuilder"
     print(f"\n\n{'=' * (len(welcome_message)+4)}")
     print(f"| {welcome_message} |")
     print(f"{'=' * (len(welcome_message)+4)}\n\n")
 
+def get_name():
     print("Please enter the name of your experimental protocol: ")
-    protocol_name = input(" => ")
+    return input(" => ")
 
-    workbook  = xlsxwriter.Workbook(f"{protocol_name}.xlsx")
-    worksheet = workbook.add_worksheet()
-
+def get_conditions():
     print("\nPlease experimental conditions/variables separated by commas:")
     print("For example: Bed Elevation, Patient Orientation")
-    conditions = input(" => ").split(",")
+    return input(" => ").split(",")
+
+def create_header(worksheet, conditions):
 
     alphabet = [chr(i) for i in range(ord('A'), ord('Z')+1)]
     header_cells = [alphabet[i] + str(i+1) for i in range(len(conditions))]
 
     for ix, condition in enumerate(conditions):
         worksheet.write(f"{alphabet[ix]}1", condition)
-
-    workbook.close()
 
 if __name__ == "__main__":
     main()
